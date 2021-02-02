720520dae134415e6c0d3e472bb6973763809498
Zhijiang Yang <yzjkid9@gmail.com>
2021-02-02 22:48:28 +0800

    Site updated: 2021-02-02 22:48:25

--- sodokan/source/js/script.js
+++ sodokan/source/js/script.js
@@ -3,20 +3,64 @@
  * @Author: Kotori Y
  * @Date: 2021-02-01 16:03:15
  * @LastEditors: Kotori Y
- * @LastEditTime: 2021-02-01 22:59:13
+ * @LastEditTime: 2021-02-02 22:46:02
  * @FilePath: \kotori-sokoban\source\js\script.js
  * @AuthorMail: kotori@cbdd.me
  */
 
+async function sleeper(delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, delay);
+  });
+}
+
 class Sokoban {
-  constructor(gameArea, borderArea, boxArea, goalArea, humanArea) {
+  constructor(
+    gameArea,
+    borderArea,
+    boxArea,
+    goalArea,
+    humanArea,
+    infoElem,
+    moveElem,
+    timeElem,
+    clearElem,
+    maxStageNum
+  ) {
     this.gameArea = gameArea;
     this.borderArea = borderArea;
     this.boxArea = boxArea;
     this.goalArea = goalArea;
     this.humanArea = humanArea;
+    this.infoElem = infoElem;
+    this.moveElem = moveElem;
+    this.timeElem = timeElem;
+    this.clearElem = clearElem;
+    this.moveNum = 0;
+    this.timer = null;
+    this.timeCost = 0; // unit is second
+    this.addNum = false;
+    this.allowedMove = true;
+    this.traceHistory = {};
+    this.undo = false;
+    this.stageNum = 1;
+    this.maxStageNum = maxStageNum;
 
+    document.addEventListener("keydown", (e) => this.#eventFunc(e), false);
     this.start();
+  }
+
+  #recordTrace() {
+    this.human = this.humanArea.children[0];
+    this.boxes = this.boxArea.children;
+
+    var history = [[this.human.offsetLeft, this.human.offsetTop]];
+    for (let box of this.boxes) {
+      history.push([box.offsetLeft, box.offsetTop]);
+    }
+    this.traceHistory[this.moveNum] = history;
   }
 
   async #getStageData(stageNum) {
@@ -31,9 +75,9 @@ class Sokoban {
       var elem = document.createElement("div");
       elem.classList = elementType;
       var [x, y] = locates;
+
       elem.style.left = `${x * this.gridSize}px`;
       elem.style.top = `${height - (y + 1) * this.gridSize}px`;
-
       elems.push(elem);
     }
     return elems;
@@ -41,6 +85,7 @@ class Sokoban {
 
   async #generateStage(stageNum) {
     var stage = await this.#getStageData(stageNum);
+    this.moveNum = 0;
     this.gridSize = stage["gridSize"];
     var width = stage["width"] * this.gridSize;
     var height = stage["height"] * this.gridSize;
@@ -64,6 +109,32 @@ class Sokoban {
     values[3].forEach((element) => {
       this.humanArea.appendChild(element);
     });
+
+    this.borders = this.borderArea.children;
+    this.goals = this.goalArea.children;
+    this.#recordTrace();
+
+    this.infoElem.style.left = `${width + this.gridSize}px`;
+    document.querySelector("#stageNum").innerHTML = stageNum;
+  }
+
+  #updateNum() {
+    if (this.addNum) {
+      this.moveElem.innerHTML = this.moveNum;
+    }
+  }
+
+  #updateTime() {
+    this.timer = setInterval(() => {
+      this.timeCost++;
+
+      var m = parseInt(this.timeCost / 60);
+      var s = this.timeCost % 60;
+      var m = m < 10 ? `0${m}` : `${m}`;
+      var s = s < 10 ? `0${s}` : `${s}`;
+
+      this.timeElem.innerHTML = `${m}:${s}`;
+    }, 1000);
   }
 
   #isColliding(left, top, obstacle) {
@@ -90,26 +161,119 @@ class Sokoban {
     return aimBox;
   }
 
+  #isFinised() {
+    var num = document.querySelectorAll(".box.active").length;
+    return num === this.boxes.length;
+  }
+
+  #nextStage() {
+    this.stageNum++;
+    if (this.stageNum <= this.maxStageNum) {
+      this.traceHistory = {};
+      this.borderArea.innerHTML = "";
+      this.boxArea.innerHTML = "";
+      this.humanArea.innerHTML = "";
+      this.goalArea.innerHTML = "";
+
+      this.start();
+      this.moveNum = 0;
+      this.timeCost = 0;
+      this.#updateNum();
+
+      this.gameArea.style.opacity = 1;
+      this.clearElem.style.visibility = "hidden";
+      this.clearElem.style.opacity = 0;
+
+      this.allowedMove = true;
+    } else {
+      this.stageNum--
+    }
+  }
+
+  async #checkBox(aimBox) {
+    aimBox.classList.remove("active");
+    if (
+      this.#isTouchObstacle(aimBox.offsetLeft, aimBox.offsetTop, this.goals)
+    ) {
+      aimBox.classList.add("active");
+      if (this.#isFinised()) {
+        clearInterval(this.timer);
+        this.allowedMove = false;
+
+        this.gameArea.style.opacity = 0.2;
+        this.clearElem.style.height = `${this.gameArea.clientHeight}px`;
+        this.clearElem.style.width = `${this.gameArea.clientWidth}px`;
+        this.clearElem.style.visibility = "visible";
+        this.clearElem.style.opacity = 1;
+
+        await sleeper(1500);
+
+        this.#nextStage();
+      }
+    }
+  }
+
   #keyEvent(e) {
     switch (e.code) {
       case "ArrowUp":
       case "KeyW":
         this.direction = 2;
+        this.addNum = true;
         break;
       case "ArrowDown":
       case "KeyS":
         this.direction = 8;
+        this.addNum = true;
         break;
       case "ArrowLeft":
       case "KeyA":
         this.direction = 4;
+        this.addNum = true;
         break;
       case "ArrowRight":
       case "KeyD":
         this.direction = 6;
+        this.addNum = true;
+        break;
+      case "KeyQ":
+        this.direction = -1;
+        this.addNum = true;
+        break;
+      case "KeyE":
+        this.direction = 0;
+        this.addNum = true;
         break;
       default:
-        this.direction = this.direction;
+        this.direction = -2;
+        this.addNum = false;
+    }
+  }
+
+  #undo() {
+    if (this.direction === -1) {
+      this.moveNum--;
+      this.moveNum = this.moveNum >= 0 ? this.moveNum : 0;
+    } else {
+      var maxTrace = Object.keys(this.traceHistory).length - 1;
+      // console.log(maxTrace);
+      this.moveNum++;
+      this.moveNum = this.moveNum <= maxTrace ? this.moveNum : maxTrace;
+    }
+
+    var temp = this.traceHistory[this.moveNum];
+
+    this.human.style.left = `${temp[0][0]}px`;
+    this.human.style.top = `${temp[0][1]}px`;
+
+    for (let idx = 1; idx < temp.length; idx++) {
+      var [left, top] = temp[idx];
+      var box = this.boxes[idx - 1];
+
+      if (box.offsetLeft !== left || box.offsetTop !== top) {
+        box.style.left = `${left}px`;
+        box.style.top = `${top}px`;
+        this.#checkBox(box);
+      }
     }
   }
 
@@ -137,51 +301,80 @@ class Sokoban {
   }
 
   #controller() {
-    var human = document.querySelector(".human");
-    var borders = this.borderArea.children;
-    var goals = this.goalArea.children;
-    var boxes = this.boxArea.children;
+    switch (this.direction) {
+      case -1:
+      case 0:
+        this.#undo();
+        break;
+      case 2:
+      case 8:
+      case 4:
+      case 6:
+        var [hLeft, hTop] = this.#move(this.human);
 
-    var [hLeft, hTop] = this.#move(human);
+        var aimBox = this.#isPushBox(hLeft, hTop, this.boxes);
 
-    var aimBox = this.#isPushBox(hLeft, hTop, boxes);
-
-    if (aimBox) {
-      var [bLeft, bTop] = this.#move(aimBox);
-      if (
-        !this.#isTouchObstacle(bLeft, bTop, borders) &&
-        !this.#isTouchObstacle(bLeft, bTop, boxes)
-      ) {
-        aimBox.style.left = `${bLeft}px`;
-        aimBox.style.top = `${bTop}px`;
-        aimBox.classList.remove("active")
-        if (this.#isTouchObstacle(bLeft, bTop, goals)) {
-          aimBox.classList.add("active")
+        if (aimBox) {
+          var [bLeft, bTop] = this.#move(aimBox);
+          if (
+            !this.#isTouchObstacle(bLeft, bTop, this.borders) &&
+            !this.#isTouchObstacle(bLeft, bTop, this.boxes)
+          ) {
+            aimBox.style.left = `${bLeft}px`;
+            aimBox.style.top = `${bTop}px`;
+            this.#checkBox(aimBox);
+            this.human.style.left = `${hLeft}px`;
+            this.human.style.top = `${hTop}px`;
+            this.moveNum++;
+          }
+        } else {
+          if (!this.#isTouchObstacle(hLeft, hTop, this.borders)) {
+            this.human.style.left = `${hLeft}px`;
+            this.human.style.top = `${hTop}px`;
+            this.moveNum++;
+          }
         }
-        human.style.left = `${hLeft}px`;
-        human.style.top = `${hTop}px`;
-      }
-    } else {
-      if (!this.#isTouchObstacle(hLeft, hTop, borders)) {
-        human.style.left = `${hLeft}px`;
-        human.style.top = `${hTop}px`;
-      }
+        break;
+      default:
+        break;
+    }
+    this.#updateNum();
+    this.#recordTrace();
+  }
+
+  #eventFunc(e) {
+    if (this.allowedMove) {
+      this.#keyEvent(e);
+      this.#controller();
     }
   }
 
   async start() {
-    await this.#generateStage(1);
-    document.addEventListener("keydown", (e) => {
-      this.#keyEvent(e);
-      this.#controller();
-    });
+    await this.#generateStage(this.stageNum);
+    this.#updateTime();
   }
 }
 
+const maxStageNum = 2
 var borderArea = document.querySelector(".border-container");
 var boxArea = document.querySelector(".box-container");
 var goalArea = document.querySelector(".goal-container");
 var humanArea = document.querySelector(".human-container");
+var infoElem = document.querySelector(".info-container");
+var moveElem = document.querySelector("#moveNum");
+var timeElem = document.querySelector("#timeCost");
+var clearElem = document.querySelector(".game-clear");
 var gameArea = document.querySelector(".game-area");
 
-var app = new Sokoban(gameArea, borderArea, boxArea, goalArea, humanArea);
+var app = new Sokoban(
+  gameArea,
+  borderArea,
+  boxArea,
+  goalArea,
+  humanArea,
+  infoElem,
+  moveElem,
+  timeElem,
+  clearElem,
+  maxStageNum,
+);
